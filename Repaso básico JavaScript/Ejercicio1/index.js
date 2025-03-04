a113dbbeaed6d6589b45e279f1ec64ed1ef677b3
alejgalvez <alejgalvezg@gmail.com>
2025-03-04 17:54:21 +0100

    Lenguaje de Marcas

---
+++ Repaso básico JavaScript/Ejercicio1/index.js
@@ -0,0 +1,9 @@
+var nombre = "Alejandro"
+var apellido = "Galvez"
+var altura = 1.73
+console.log(nombre)
+console.log(apellido)
+console.log(altura)
+
+console.log(typeof(nombre)) //ver el valor de la variable (si es int, String...)
+console.log(typeof(altura))
---
+++ Repaso básico JavaScript/Ejercicio2/index.js
@@ -0,0 +1,9 @@
+window.alert("¡Hola Mundo!");
+var nombre = window.prompt("Por favor, introduce tu nombre:");
+// si proporciono un nombre, se escribirá en consola
+if (nombre) {
+console.log("Hola, " + nombre);
+// sino proporciono un nombre, se escribirá en consola lo siguiente
+} else {
+console.log("No se proporcionó un nombre");
+}
